f02276333f11b821878dd3cc1b897986ce88b5d1
Rohit <rohitpingale103@gmail.com>
2020-12-16 13:32:01 +0530

    calculating distacne

--- script.js
+++ script.js
@@ -92,7 +92,8 @@ function renderPlaces(places,position) {
           var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
             Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
           var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-          var d = (double) Math.round(R * c * 100.0)/ 100.0;
+          var d =  (R * c);
+          d = d.toFixed(2);
           return d;
         }
 
