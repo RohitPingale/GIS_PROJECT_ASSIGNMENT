052165e4f546d41fd41a717d3067110a520bdf19
Rohit <rohitpingale103@gmail.com>
2020-12-16 13:23:08 +0530

    calculating distacne

--- script.js
+++ script.js
@@ -80,6 +80,28 @@ function dynamicLoadPlaces(position) {
 function renderPlaces(places,position) {
     let scene = document.querySelector('a-scene');
 
+    // distance between them as the crow flies (in km)
+     function calcCrow(lat1, lon1, lat2, lon2) 
+        {
+          var R = 6371; // km
+          var dLat = toRad(lat2-lat1);
+          var dLon = toRad(lon2-lon1);
+          var lat1 = toRad(lat1);
+          var lat2 = toRad(lat2);
+
+          var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+          var d = Math.round(R * c * 100.0)/ 100.0;
+          return d;
+        }
+
+        // Converts numeric degrees to radians
+        function toRad(Value) 
+        {
+            return Value * Math.PI / 180;
+        }
+
     places.forEach((place) => {
         const latitude = place.location.lat;
         const longitude = place.location.lng;
@@ -103,27 +125,6 @@ function renderPlaces(places,position) {
         
         scene.appendChild(text);
         
-       // distance between them as the crow flies (in km)
-        function calcCrow(lat1, lon1, lat2, lon2) 
-        {
-          var R = 6371; // km
-          var dLat = toRad(lat2-lat1);
-          var dLon = toRad(lon2-lon1);
-          var lat1 = toRad(lat1);
-          var lat2 = toRad(lat2);
-
-          var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-          var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-          var d = R * c;
-          return d;
-        }
-
-        // Converts numeric degrees to radians
-        function toRad(Value) 
-        {
-            return Value * Math.PI / 180;
-        }
 
         // add place icon
         console.log(calcCrow(latitude,longitude,position.latitude,position.longitude));
